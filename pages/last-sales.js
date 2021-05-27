ea2fb80a85fd34710af29db38684c20a8d043a53
JulioAvalos <julioavalos2@msn.com>
2021-05-26 19:47:44 -0600

    implementing client-side data fetching

---
+++ pages/last-sales.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+function LastSalesPage(props) {
+  const [sales, setSales] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(props.url)
+      .then((response) => response.json())
+      .then((data) => {
+        const transformedSales = [];
+
+        for (const key in data) {
+          transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          });
+        }
+
+        setSales(transformedSales);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!sales) {
+    return <p>No data yet</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - {sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default LastSalesPage;
+
+//added to retrieve my .env backend url
+export async function getStaticProps() {
+  return {
+    props: {
+      url: process.env.BACKEND_URL,
+    },
+  };
+}

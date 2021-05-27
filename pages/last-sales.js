130fd776ab70dc2b9860e41c4da9de0a531a587e
JulioAvalos <julioavalos2@msn.com>
2021-05-26 23:22:33 -0600

    using the "useSWR" NextJS Hook

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,35 +1,53 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalesPage(props) {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(props.url);
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(props.url)
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+    if(data) {
+      const transformedSales = [];
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(props.url)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Failed to load.</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
